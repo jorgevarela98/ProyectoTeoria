0e9e0e0b71dadcdd2ff8890db8198f4fb2e09a50
jorgevarela98 <jormivaroli98@hotmail.com>
2022-08-03 21:32:55 -0600

    Burgos, Sevilla y Baca

--- ejercicio2/backend/index.js
+++ ejercicio2/backend/index.js
@@ -2,3 +2,22 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const cors = require('cors')
+require('dotenv').config()
+
+app.use(cors({credentials: true, origin: '*'}));
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
+
+
+
+app.get('/', (req,res)=>{
+    res.send('Los meros Sevillistas');
+})
+
+
+puerto = 8088 || process.env.puerto;
+host = 'localhost' || process.env.host;
+
+app.listen(puerto, host, ()=>{
+    console.log(`Servidor corriendo en http://${host}:${puerto}`)
+})
