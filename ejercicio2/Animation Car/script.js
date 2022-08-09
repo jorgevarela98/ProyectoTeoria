db86cdc2349436114798489f07482f875648c22f
jorgevarela98 <jormivaroli98@hotmail.com>
2022-08-08 19:22:12 -0600

    agregado de pausa y continuar animacion

--- ejercicio2/Animation Car/script.js
+++ ejercicio2/Animation Car/script.js
@@ -1,4 +1,42 @@
+//Sirve para el Audio del carro (NO TOCAR)
 var aud=document.createElement('audio');
 aud.setAttribute('src','sound.mp4');
 aud.loop=true;
 aud.play();
+
+//Script para poder manipular el movimiento del carro 
+
+function pararAnimacion(){
+
+    /*
+    Pausar
+    
+    */ 
+
+
+    document.getElementById('pista').classList.remove('track');
+    document.getElementById('llanta').classList.remove('wheel');
+    document.getElementById('llanta2').classList.remove('wheel');
+    document.getElementById('carro').classList.remove('car');
+
+
+    document.getElementById('pista').classList.add('trackstop');
+    document.getElementById('llanta').classList.add('wheelstop');
+    document.getElementById('llanta2').classList.add('wheelstop');
+    document.getElementById('carro').classList.add('carstop');
+}
+function seguirAnimacion(){
+
+    document.getElementById('pista').classList.remove('trackstop');
+    document.getElementById('llanta').classList.remove('wheelstop');
+    document.getElementById('llanta2').classList.remove('wheelstop');
+    document.getElementById('carro').classList.remove('carstop');
+    /*Quitar Pausa*/
+
+
+
+    document.getElementById('pista').classList.add('track');
+    document.getElementById('llanta').classList.add('wheel');
+    document.getElementById('llanta2').classList.add('wheel');
+    document.getElementById('carro').classList.add('car');
+}
