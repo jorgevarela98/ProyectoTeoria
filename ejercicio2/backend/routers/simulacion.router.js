2959f243e0015ca28ab7e787cb547aa08923f518
jorgevarela98 <jormivaroli98@hotmail.com>
2022-08-13 21:54:47 -0600

    fasdfasdjlfasdk

--- ejercicio2/backend/routers/simulacion.router.js
+++ ejercicio2/backend/routers/simulacion.router.js
@@ -7,7 +7,7 @@ router.post('/',(req,res)=>{
     res.send('Guarda Simulacion en la base de datos del proyecto, Chomines');
 });
 
-router.get('',(req,res)=>{
+router.get('/simulaciones',(req,res)=>{
     res.send('Trae los datos de las simulaciones de la base de datos');
 });
 
