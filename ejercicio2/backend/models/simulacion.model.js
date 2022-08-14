4b0543710c4de1b133c254ee409e01c06341b106
jorgevarela98 <jormivaroli98@hotmail.com>
2022-08-13 21:52:59 -0600

    fasdfasdjlfasdk

--- ejercicio2/backend/models/simulacion.model.js
+++ ejercicio2/backend/models/simulacion.model.js
@@ -2,7 +2,22 @@ const bdConfig = require ('../config/bd.config');
 const mssql = require ('mssql');
 
 
+const insertarSimulacion = async()=>{
+    let pool; 
+    try {
+        pool = await mssql.connect(bdConfig.bdconfig);
+        let simulacion = pool.request()
+            .input()
+            .query();    
+    } catch (error){
+        console.log(`Error en la base de datos: ${error}`)
+    }finally{
+        pool.close();
+    }
+    
+}
 
 
-
-module.exports={}
+module.exports={
+    insertarSimulacion
+}
