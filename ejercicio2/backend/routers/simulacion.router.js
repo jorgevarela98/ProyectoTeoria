d73d08a2f24c5fd7b7fa74ab01f10abbfc698448
jorgevarela98 <jormivaroli98@hotmail.com>
2022-08-13 21:17:44 -0600

    fasdfasdjlfasdk

--- ejercicio2/backend/routers/simulacion.router.js
+++ ejercicio2/backend/routers/simulacion.router.js
@@ -3,6 +3,15 @@ const router = express.Router()
 const simModel = require('../models/simulacion.model')
 
 
+router.post('/',(req,res)=>{
+    res.send('Guarda Simulacion en la base de datos del proyecto, Chomines');
+});
+
+router.get('',(req,res)=>{
+    res.send('Trae los datos de las simulaciones de la base de datos');
+});
+
+
 
 
 module.exports={router};
